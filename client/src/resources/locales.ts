d3ab83ddf7c459b3c2bbc3821a1245af82d277e9
mendesjar <ga0301@outlook.com>
2024-09-26 14:35:53 -0300

    feat: locales url server

--- client/src/resources/locales.ts
+++ client/src/resources/locales.ts
@@ -2,6 +2,7 @@ const locales = {
   enviroment: import.meta.env.VITE_ENVIROMENT,
   socketApi: import.meta.env.VITE_SOCKET_API,
   socketHost: import.meta.env.VITE_SOCKET_HOSTNAME,
+  serverUrl: import.meta.env.VITE_SERVER_URL,
   socketPort: import.meta.env.VITE_SOCKET_PORT,
 };
 
